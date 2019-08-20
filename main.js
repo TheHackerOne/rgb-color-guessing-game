9901db826355f29fd1e47c08750c6c4d4792b5f0
yash chaudhary <yash2200@gmail.com>
2019-08-21 00:10:03 +0530

    Changes to be committed:

--- main.js
+++ main.js
@@ -9,6 +9,16 @@ function pick_color() {
     var random = Math.floor(Math.random() * colors.length);
     return colors[random];
 }
+// var squares=document.querySelectorAll(".square");
+// var colors=random_color_array(6);
+// var picked_color=pick_color();
+// var clicked_color;
+// var text_rgba=document.querySelector('h1');
+// text_rgba.textContent=picked_color;
+// function pick_color() {
+//     var random = Math.floor(Math.random() * colors.length);
+//     return colors[random];
+// }
 
 for(var j=0;j<squares.length;j++)
 {
