986f7fe7a5c6194c2f61c8bd7d6b47e659b0abf4
yash chaudhary <yash2200@gmail.com>
2019-08-23 22:26:44 +0530

    Changes to be committed:

--- main.js
+++ main.js
@@ -1,24 +1,48 @@
-
+var result=document.querySelector(".result");
+var reset=document.querySelector(".reset");
 var squares=document.querySelectorAll(".square");
 var colors=random_color_array(6);
 var picked_color=pick_color();
 var clicked_color;
 var text_rgba=document.querySelector('h1');
 text_rgba.textContent=picked_color;
+var easy=document.querySelector('.easy');
+var hard=document.querySelector('.hard');
+easy.addEventListener("click",function(){
+    var main_ans = [];
+    var main_ans=random_color_array(2);
+    
+    for(var u=0;u<6;u++){
+        
+        if(squares[u].style.background==picked_color)
+        {
+            var ans=u;
+            main_ans.push(squares[u].style.background);
+        }
+    }
+    for(var p=0;p<6;p++){
+        squares[p].style.display="none";
+    }
+    for(p=0;p<3;p++)
+    {
+        squares[p].style.background=main_ans[p];
+        squares[p].style.display="inline";
+    }
+})
+hard.addEventListener("click",function(){
+
+    for(var r=0;r<6;r++){
+        squares[r].style.display='inline';
+
+    }
+})
+reset.addEventListener("click",function(){
+    document.location.reload();
+})
 function pick_color() {
     var random = Math.floor(Math.random() * colors.length);
     return colors[random];
 }
-// var squares=document.querySelectorAll(".square");
-// var colors=random_color_array(6);
-// var picked_color=pick_color();
-// var clicked_color;
-// var text_rgba=document.querySelector('h1');
-// text_rgba.textContent=picked_color;
-// function pick_color() {
-//     var random = Math.floor(Math.random() * colors.length);
-//     return colors[random];
-// }
 
 for(var j=0;j<squares.length;j++)
 {
@@ -27,11 +51,20 @@ for(var j=0;j<squares.length;j++)
     squares[j].addEventListener("click",function(){
         clicked_color=this.style.background;
         if (clicked_color == picked_color) {
-            alert("CORRECT");
+            swal("CORRECT!", "You nailed it!", "success");
+            result.style.color = "blueviolet";
+            result.style.fontWeight = 800;
+            result.style.fontSize = '1.2rem';
             return;
         }
         else
-            alert("TRY AGAIN");
+        {
+            result.textContent = "TRY AGAIN";
+            result.style.color = "blueviolet";
+            result.style.fontWeight = 800;
+            result.style.fontSize = '1.2rem';
+            this.style.background='white';
+        }
 
     })
 }
